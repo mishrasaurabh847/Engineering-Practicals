28d5f9e3fdc061e4a256752bc7fdfcf8cf3dba68
mishrasaurabh847 <59078843+mishrasaurabh847@users.noreply.github.com>
2020-11-20 22:53:39 +0530

    Delete DSIP Mini Project.py

--- Semester 7/Digital Signal and Image Processing/DSIP Mini Project.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import pytesseract
-
-im = Image.open("sample.jpg")
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Saurabh R. Mishra\AppData\Local\Programs\Python\Python38\tesseract.exe'
-
-text = pytesseract.image_to_string(im, lang = 'eng')
-
-print(text)
