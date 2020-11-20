e7bcffcaf2c2c2079d35f72ca8334c52e722ef8d
mishrasaurabh847 <59078843+mishrasaurabh847@users.noreply.github.com>
2020-11-20 22:55:17 +0530

    Update DSIP Mini Project.py

--- Semester 7/Digital Signal and Image Processing/Dsip Mini Project Text Recognition in Images/DSIP Mini Project.py
+++ Semester 7/Digital Signal and Image Processing/Dsip Mini Project Text Recognition in Images/DSIP Mini Project.py
@@ -7,3 +7,14 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Saurabh R. Mishra\AppData\Loc
 text = pytesseract.image_to_string(im, lang = 'eng')
 
 print(text)
+
+'''
+OUTPUT:
+= RESTART: C:\Users\Saurabh R. Mishra\AppData\Local\Programs\Python\Python38\DSIP Mini Project.py
+MISHRA SAURABH RAJESH
+BE COMP A 64
+
+DSIP MINI PROJECT
+
+Text Recognition in Images
+'''
